d73cc72d0349659eae2b6457b53a2343ca01a970
Alejandro Gamino <alejandrogamino@pm.me>
2023-10-22 14:27:23 +0200

    Fix import

--- src/main/java/rpg/solo/oracles/mune/restservice/MUNEController.java
+++ src/main/java/rpg/solo/oracles/mune/restservice/MUNEController.java
@@ -2,7 +2,10 @@ package rpg.solo.oracles.mune.restservice;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import rpg.solo.oracles.mune.util.RequestParamsUtils;
 
 import java.util.ArrayList;
